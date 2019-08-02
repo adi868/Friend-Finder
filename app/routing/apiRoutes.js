535887c48227107293c5472bd160eb7c41e17b29
alciancio <alcianciotto@gmail.com>
2019-08-01 20:42:43 -0500

    Re-working back end

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,9 +19,60 @@ module.exports = function (app) {
     // ...the JSON is pushed to the appropriate JavaScript array
 
     // Adding new friend information via POST request
-    app.post("/api/friends", function (request, response) {
-        var newFriend = request.body;
-        friendData.push(newFriend)
-        response.json(newFriend)
+    app.post("/api/friends", function (req, res) {
+        
+        var userInputMessageBodyObject = req.body;
+       
+        ​
+                //store the total difference
+                var totalDifference=0;
+        ​
+                //store the result for each substraction from the scores
+                var result=0;
+        ​
+                //make a new array to store another array of obects with
+                //total difference in the scores
+                var newTablewithTotalDifference = [];
+                
+        ​
+                //outer for loop to iterate the entire array of object from table
+                for(var i=0; i< friendData.length;i++){
+        ​
+                    //reset the total difference for the next object person
+                    totalDifference=0;
+        ​
+                    //inner for loop to calculate the new Person from the user Input off req.body
+                    for(var j=0; j< userInputMessageBodyObject.scores.length;j++){
+        ​
+                        //subtract each object score from first element to the last element of the array
+                         result=  parseFloat(friendData[i].scores[j]) - parseFloat(userInputMessageBodyObject.scores[j])
+                    
+                         //sum up the subtraction and stores in totatdifference
+                        //and change the values to be positive
+                         totalDifference += Math.abs(result)
+                     }
+                    
+                    //inserts into a new array of object for sorting the least total difference
+                     newTablewithTotalDifference.push({
+                         name: friendData[i].name,
+                         totalDifference: totalDifference,
+                         photo: friendData[i].photo
+                     })
+                }  
+        ​
+              //add a new person to our table from 
+                //userInput that is stored in req.body
+                friendData.push(userInputMessageBodyObject);
+             
+                //sorts the new array of object against the total difference
+                newTablewithTotalDifference.sort(function(a, b){
+                    return a.totalDifference-b.totalDifference
+                })
+        ​
+                console.log(newTablewithTotalDifference)
+                
+                //returns first element and response back the total least difference to the           //client during the post request 
+                res.json(newTablewithTotalDifference[0]);
+
     })
 }
