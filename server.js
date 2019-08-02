c41fa20d4c2d52dbddb3e7c509d8a946fa5a7a89
alciancio <alcianciotto@gmail.com>
2019-08-01 22:42:34 -0500

    Updated back-end working and modal

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var express = require("express");
 //This tells node that we are creating an "express" server
 var app = express();
 //Sets an initial port for listener
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 //Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-app.use(express.static(__dirname + '/app/public'));
+app.use(express.static('app/public'));
 
 //Router. Points the server to a series of "route" files
 //These routes give our server a "map" of how to respond when users visit or request data from various URLs.
